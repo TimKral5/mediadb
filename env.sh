afb66e871302be8e06cb212b5832590632a03508
Tim Kral <mail@timkral.com>
2025-06-14 20:48:38 +0200

    add missing environment variable

--- env.sh
+++
@@ -1,8 +0,0 @@
-export MEDIADB_LDAP_ADDRESS=ldap://localhost:389
-export MEDIADB_BASE_DN=dc=example,dc=org
-export MEDIADB_BIND_DN=cn=admin,dc=example,dc=org
-export MEDIADB_BIND_PASSWORD=admin
-export MEDIADB_GROUP_DN=ou=groups,dc=example,dc=org
-export MEDIADB_USER_DN=ou=groups,dc=example,dc=org
-export MEDIADB_HTTP_ADDRESS=:3000
-
