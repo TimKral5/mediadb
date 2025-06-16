3d28d4473cd547f1d21a09df610506f956cdca71
Tim Kral <mail@timkral.com>
2025-06-16 11:17:20 +0200

    create env.sh to run the project locally

---
+++ env.sh
@@ -0,0 +1,9 @@
+export MEDIADB_LDAP_ADDRESS=ldap://localhost:389
+export MEDIADB_BASE_DN=dc=example,dc=org
+export MEDIADB_BIND_DN=cn=admin,dc=example,dc=org
+export MEDIADB_BIND_PASSWORD=admin
+export MEDIADB_GROUP_DN=ou=groups,dc=example,dc=org
+export MEDIADB_USER_DN=ou=users,dc=example,dc=org
+export MEDIADB_MONGO_URL=mongodb://root:root@localhost/
+export MEDIADB_HTTP_ADDRESS=:3001
+
